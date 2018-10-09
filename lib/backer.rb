4530b59ba417cc86edb84fe52bb49459c7dcff41
Manny Pissanos <mpissano@gmail.com>
2018-10-09 19:08:07 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,8 +2,13 @@ class Backer
   attr_reader :name
   def initialize(name)
     @name = name
+    @backed_projects = []
   end
   
+  def back_project(project)
+    @backed_projects << project
+  end
+    
   
   
   
