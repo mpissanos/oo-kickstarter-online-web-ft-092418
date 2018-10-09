42b785f9beb65c617ffeec1e49a19bbb0dd664e1
Manny Pissanos <mpissano@gmail.com>
2018-10-09 19:09:49 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,12 @@
 class Project
-  attr_accessor :title
+  attr_reader :title
+  attr_accessor :backer
   def initialize(title)
     @title = title
     @backers = []
   end
   
   def add_backer(backer)
+    @backer << backer
+  end
 end
